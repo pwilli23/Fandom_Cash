cb9bda704df4f9c916b49c7e129ba45ddf61ce9f
pwilli23 <93749067+pwilli23@users.noreply.github.com>
2022-05-04 19:22:51 -0400

    Streamlit update #1, basic DataFrame loaded into file

--- streamlit.py
+++ streamlit.py
@@ -3,7 +3,18 @@ import pandas as pd
 import numpy as np
 import time
 
-st.write("# Welcome to the Fandom Cash Exchange: Building the Brand One Token at a Time!")
+st.title(" Welcome to the Fandom Cash Exchange: Building the Brand One Token at a Time!")
 
+df = pd.read_csv("Fandom_items.csv", index_col = "item", parse_dates=True, infer_datetime_format = True)
+
+def color_negative(val):
+    color = 'green' if val else 'red'
+    return f'background-color: {color}'
+
+df.sort_index(inplace=True, ascending = False)
+
+start = st.selectbox("Select a Fan Experience item:", df.index)
+
+st.table(df)
 
 
